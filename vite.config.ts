e0de1e7c39443bdc1c577a150b81ed51009f70ed
ramjiram42 <85981244+ramjiram42@users.noreply.github.com>
2026-03-18 00:50:36 +0530

    Increased chunkSizeWarningLimit to 2000 to resolve deployment warnings

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
